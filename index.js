924e57f815db5f508cb0939e68b17f973853b362
我也不太了解你 <yaozhi614@gmail.com>
2026-09-18 23:06:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -276,9 +276,9 @@
     container.id = CONFIG.ID;
     container.style.cssText = `
         position: fixed; top: 0; left: 0;
-        width: 0; height: 0;
-        min-width: 0; min-height: 0;
-        overflow: visible; pointer-events: auto; z-index: ${CONFIG.Z_INDEX};
+        width: 100vw; height: 100dvh;
+        min-width: 100vw; min-height: 100vh;
+        overflow: visible; pointer-events: none; z-index: ${CONFIG.Z_INDEX};
     `;
     targetDoc.body.appendChild(container);
 
